caf8a67662f9a5a8e24144ab134bc7c658e91671
lovekwe <jessiezhao1816@gmail.com>
2023-03-07 11:07:55 +1100

    add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import memesData from './memesData'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [url, setUrl] = useState("")
+  const memesArray = memesData.data.memes
+  const randomNumber = Math.floor(Math.random()*memesArray.length)
 
+  function generatePic(){
+    setUrl(memesArray[randomNumber].url)
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className='form'>
+       <input className="input" type="text" placeholder='TopText'/>
+       <input className="input" type="text" placeholder='bottomText'/>
+       <button className='button' onClick={generatePic}>Get a new meme image</button> 
+       <img className="image" src={url} alt="pic" />
     </div>
   )
 }
