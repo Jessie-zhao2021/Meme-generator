4371d1d4da06b945abb50ead951fc3fe345d83d4
lovekwe <jessiezhao1816@gmail.com>
2023-03-23 11:32:55 +1100

    add text function

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,45 @@ import './App.css'
 import memesData from './memesData'
 
 function App() {
-  const [url, setUrl] = useState("")
+  const [meme, setMeme] = useState({
+    topText:"",
+    bottomText:"",
+    memeImage:"https://i.imgflip.com/28j0te.jpg"
+  })
   const memesArray = memesData.data.memes
   const randomNumber = Math.floor(Math.random()*memesArray.length)
 
   function generatePic(){
-    setUrl(memesArray[randomNumber].url)
+   
+    setMeme(prevMeme =>({
+      ...prevMeme,
+      memeImage: memesArray[randomNumber].url
+    }))
+  }
+
+  function handleChange(event){
+    const{value, name} = event.target
+    setMeme(prevMeme=>({
+      ...prevMeme,
+      [name]:value
+    }))
   }
   return (
-    <div className='form'>
-       <input className="input" type="text" placeholder='TopText'/>
-       <input className="input" type="text" placeholder='bottomText'/>
-       <button className='button' onClick={generatePic}>Get a new meme image</button> 
-       <img className="image" src={url} alt="pic" />
+    <div className='main'>
+      <div className='form'>
+        <input className="input" type="text" placeholder='TopText'
+               value={meme.topText} name="topText" onChange={handleChange}
+        />
+        <input className="input" type="text" placeholder='bottomText'
+               value={meme.bottomText} name="bottomText" onChange={handleChange}
+        />
+        <button className='button' onClick={generatePic}>Get a new meme image</button> 
+      </div>
+      <div className="meme">
+        <img className="image" src={meme.memeImage} alt="pic" />
+        <h2 className='text top'>{meme.topText}</h2>
+        <h2 className='text bottom'>{meme.bottomText}</h2>        
+      </div>
     </div>
   )
 }
